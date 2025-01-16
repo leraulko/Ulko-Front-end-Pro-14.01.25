e8191028cd7ac44ae7cc624724398aee4ab59ea3
Lera Ulko <leraulko@MacBook-Pro-Lera.local>
2025-01-17 00:24:23 +0200

    change of calculations

--- HW2/index.js
+++ HW2/index.js
@@ -2,7 +2,7 @@ let userHours = prompt('Enter the number of hours');
 
 console.log(userHours);
 
-let convertedHours = +userHours * 60;
+let convertedHours = +userHours * 60 * 60;
 
 console.log(convertedHours);
 
