c495d1ee98c22c6112341692c3653f54c90d4342
Lera Ulko <leraulko@MacBook-Pro-Lera.local>
2025-01-17 00:08:23 +0200

    added 2nd homework

---
+++ HW2/index.js
@@ -0,0 +1,9 @@
+let userHours = prompt('Enter the number of hours');
+
+console.log(userHours);
+
+let convertedHours = +userHours * 60;
+
+console.log(convertedHours);
+
+alert('In ' + userHours + ' hours we have ' + convertedHours + ' seconds');
